278bf3a6b00f4b63382b037e3ace78f312b8aaa7
dragonsat10 <62356885+dragonsat10@users.noreply.github.com>
2020-03-18 21:15:43 +0000

    new stream starting system

--- stream.js
+++ stream.js
@@ -1,3 +1,33 @@
+var filmStartDate = new Date(FILM_START);
+
+var enabled = false;
+
+function enableVideo() {
+  var now = new Date();
+  var enableTime = new Date(filmStartDate.getTime() - STREAM_PRE_MILLISECS);
+  var disableTime = new Date(filmStartDate.getTime() + FILM_DURATION_MILLISECS + STREAM_POST_MILLISECS);
+  var enableDistance = enableTime.getTime() - now.getTime();
+  var disableDistance = disableTime.getTime() - now.getTime();
+  if (disableDistance <= 0) {
+    if (enabled) {
+      if (player && !player.paused()) {
+        player.pause();
+      }
+      enabled = false;
+    }
+  }
+  else if (enableDistance <= 0) {
+    if (!enabled) {
+      // set the video
+      if (player) {
+        player.src({ type:'application/x-mpegURL', src: STREAM_URL });
+      }
+      enabled = true;
+    }
+  }
+}
+
+
 var options = {
   plugins: {
     httpSourceSelector: {
@@ -8,15 +38,8 @@ var options = {
 var player = videojs('dragon-stream', options);
 player.httpSourceSelector();
 
-// listen to messages from parent window
-window.addEventListener('message', function (message) {
-  if (message.data === 'pause') {
-    if (player && !player.paused()) {
-      player.pause();
-    }
-  } else if (message.data === 'play') {
-    if (player) {
-      player.src({ type:'application/x-mpegURL', src: STREAM_URL });
-    }
-  }
-}, false);
+// when player ready, do we enable it?
+player.ready(function () {
+  enableVideo();
+  setInterval(enableVideo, 500);
+})
