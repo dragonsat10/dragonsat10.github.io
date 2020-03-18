53772c0985ced988c0a9f6b6cb0b39adf2ed2908
dragonsat10 <62356885+dragonsat10@users.noreply.github.com>
2020-03-18 22:03:28 +0000

    add new test time

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ var CHAT_URL = 'https://dragonsat11-chat.herokuapp.com/'
 var STREAM_URL = 'https://dragonsat11-stream.herokuapp.com/master.m3u8';
 
 // countdown config
-var FILM_START = '2020-03-18T21:13:00.000Z';
+var FILM_START = '2020-03-18T22:20:00.000Z';
 var FILM_DURATION_MILLISECS = (0 * 60 * 60 * 1000) + (22 * 60 * 1000) + (12.29 * 1000); // 00:22:12.29 mins
 var STREAM_PRE_MILLISECS = (13 * 60 * 1000) + (0 * 1000); // 13:00.00
 var STREAM_POST_MILLISECS = (4 * 60 * 1000) + (41.02 * 1000); // 04:41.02
