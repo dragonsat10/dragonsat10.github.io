3633eda0686cf75f87f4b77823a47b85d959c355
dragonsat10 <62356885+dragonsat10@users.noreply.github.com>
2021-03-24 21:23:40 +0000

    2021 testing

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ var CHAT_URL = 'https://dragonsat10-chat.herokuapp.com/'
 var STREAM_URL = 'https://dragonsat10-stream.herokuapp.com/master.m3u8';
 
 // countdown config
-var FILM_START = '2020-03-26T22:00:00.000Z';
+var FILM_START = '2021-03-24T22:00:00.000Z';
 var FILM_DURATION_MILLISECS = (1 * 60 * 60 * 1000) + (37 * 60 * 1000) + (51.04 * 1000); // 01:37:51.04
 var STREAM_PRE_MILLISECS = (13 * 60 * 1000) + (00 * 1000); // 13:00.00
 var STREAM_POST_MILLISECS = (4 * 60 * 1000) + (0 * 1000); // 04:00.00
